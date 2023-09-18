2ee33cae00730513486a70643f53886c637269de
Carter Thomas <cartermatthewthomas@gmail.com>
2023-09-18 09:57:43 -0500

    Initial Commit

---
+++ NumberConversionApp/NumberConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberConversionAppApp.swift
+//  NumberConversionApp
+//
+//  Created by Carter M. Thomas on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct NumberConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
