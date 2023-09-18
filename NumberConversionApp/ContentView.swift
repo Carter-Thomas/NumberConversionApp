cc7059af060c12cce30a33bdcf028a2e14d7ef3d
Carter Thomas <cartermatthewthomas@gmail.com>
2023-09-18 10:36:19 -0500

    boing

--- NumberConversionApp/ContentView.swift
+++ NumberConversionApp/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var input = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Decimal to Binary Converter").font(.system(size:30))
+                .frame(width:500, alignment: .center)
+                .padding(50)
+           
+            Text("Put Number Here")
+            TextField("Number", value: $input, formatter: NumberFormatter())
+                .font(.system(size:50))
+                .multilineTextAlignment(.center)
+                .frame(width: 400,
+                       alignment: .center)
+                .padding(50)
+            Text("Output In Binary")
+            let binary = String(input, radix: 2)
+            Text(binary).font(.system(size:50))
+                .frame(width:400, alignment: .center)
+                      
         }
         .padding()
     }
