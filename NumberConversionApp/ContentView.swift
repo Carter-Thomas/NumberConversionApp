0bff95eab5a6e04de5b354afe234106d2d3a053f
Carter Thomas <cartermatthewthomas@gmail.com>
2023-09-20 10:26:25 -0500

    Fixed Content View

--- NumberConversionApp/ContentView.swift
+++ NumberConversionApp/ContentView.swift
@@ -8,30 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var input = 0
+    
+    
+    @ObservedObject var numberConversionVM = NumberConversionViewModel()
+    
     var body: some View {
-        VStack {
-            Text("Decimal to Binary Converter").font(.system(size:30))
-                .frame(width:500, alignment: .center)
-                .padding(50)
-           
-            Text("Put Number Here")
-            TextField("Number", value: $input, formatter: NumberFormatter())
-                .font(.system(size:50))
-                .multilineTextAlignment(.center)
-                .frame(width: 400,
-                       alignment: .center)
-                .padding(50)
-            Text("Output In Binary")
-            let binary = String(input, radix: 2)
-            Text(binary).font(.system(size:50))
-                .frame(width:400, alignment: .center)
-                      
+        ZStack {
+            
+            VStack {
+                Picker("Type of Conversion", selection: $numberConversionVM.selectedConversion) {
+                    ForEach(ConversionType.allCases, id: \.self) {
+                        type in Text(type.rawValue)
+                    }
+                }
+                .onChange(of: numberConversionVM.selectedConversion) {
+                    value in numberConversionVM.inputUpdate()
+                }
+                TextField("Input", text: $numberConversionVM.input)
+                    .onChange(of: numberConversionVM.input) {value in numberConversionVM.inputUpdate()}
+                .textFieldStyle(.roundedBorder)
+                
+                Text(numberConversionVM.result)
+            }
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
